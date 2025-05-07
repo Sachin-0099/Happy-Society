ad18a68abf19fcdff570203020eacdbd35b32b16
amanrawat9711 <85890950+amanrawat9711@users.noreply.github.com>
2025-05-07 18:05:19 +0530

    revert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,10 @@ import Hero from '../components/Hero'
 import About from '../components/About';
 import AboutSociety from '../components/AboutSociety';
 import Projects from '../components/Projects';
-import Header from '../components/Header';
 
 const Home = () => {
   return (
   <>
-    <Header/>
   <Hero/>
     <About/>
     <AboutSociety/>
