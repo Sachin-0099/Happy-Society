f00ece910f1de2c0ddb8a6af774fa6ae417c659e
amanrawat9711 <85890950+amanrawat9711@users.noreply.github.com>
2025-05-07 17:46:12 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ import Hero from '../components/Hero'
 import About from '../components/About';
 import AboutSociety from '../components/AboutSociety';
 import Projects from '../components/Projects';
+import Header from '../components/Header';
 
 const Home = () => {
   return (
   <>
+    <Header/>
   <Hero/>
     <About/>
     <AboutSociety/>
